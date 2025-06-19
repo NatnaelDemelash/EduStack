12926d65f22d549d1e0f4a3b5cebbdb84ed68cdb
NatnaelDemelash <natidemelash074@gmail.com>
2025-06-19 21:04:15 +0300

    dark theme and Login UI

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,34 @@
+import { buttonVariants } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
 import { ReactNode } from 'react';
+
+import Logo from '@/public/logo.png';
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col relative min-h-svh justify-center items-center">
-      <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
+      <Link
+        href="/"
+        className={buttonVariants({
+          variant: 'outline',
+          className: 'absolute top-4 left-4',
+        })}
+      >
+        <ArrowLeft className="size-4" />
+        <span>Back</span>
+      </Link>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center self-center font-medium uppercase"
+        >
+          <Image src={Logo} alt="Logo" width={60} height={60} />
+          EduStack
+        </Link>
+        {children}
+      </div>
     </div>
   );
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,6 +6,9 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { GithubIcon } from 'lucide-react';
 import React from 'react';
 
 export default function Login() {
@@ -19,7 +22,27 @@ export default function Login() {
       </CardHeader>
 
       <CardContent>
-        <Button>Sign in with GitHub</Button>
+        <Button className="w-full" variant="outline">
+          <GithubIcon className="size-4" />
+          Sign in with GitHub
+        </Button>
+
+        <div className="flex items-center gap-4 my-6">
+          <div className="flex-grow h-px bg-muted" />
+          <span className="text-muted-foreground text-sm whitespace-nowrap">
+            Or continue with
+          </span>
+          <div className="flex-grow h-px bg-muted" />
+        </div>
+
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" name="email" placeholder="email@example.com" />
+          </div>
+
+          <Button>Continue with Email</Button>
+        </div>
       </CardContent>
     </Card>
   );
