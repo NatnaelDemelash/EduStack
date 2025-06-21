d4852208404fd6931505be224a6f0725c6963fdf
NatnaelDemelash <natidemelash074@gmail.com>
2025-06-21 17:13:35 +0300

    fix: login session checking and add logout functionality

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,79 +1,16 @@
-'use client';
+import { auth } from '@/lib/auth';
+import LoginForm from './_components/LoginForm';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { authClient } from '@/lib/auth-client';
-import { GithubIcon, Loader } from 'lucide-react';
-import React, { useTransition } from 'react';
-import { toast } from 'sonner';
+export default async function Login() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-export default function Login() {
-  const [githubPending, startGithubTransition] = useTransition();
-
-  async function signInWithGithub() {
-    startGithubTransition(async () => {
-      await authClient.signIn.social({
-        provider: 'github',
-        callbackURL: '/',
-        fetchOptions: {
-          onSuccess: () => {
-            toast.success('SignedIn with Github, redirecting....');
-          },
-          onError: () => {
-            toast.error('Internal Server Error');
-          },
-        },
-      });
-    });
+  if (session) {
+    redirect('/');
   }
 
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-xl">Welcome Back!</CardTitle>
-        <CardDescription>
-          Login with your GitHub or Email Account{' '}
-        </CardDescription>
-      </CardHeader>
-
-      <CardContent>
-        <Button className="w-full" variant="outline" onClick={signInWithGithub}>
-          {githubPending ? (
-            <>
-              <Loader className="size-4" /> <span>Loading..</span>
-            </>
-          ) : (
-            <>
-              <GithubIcon className="size-4" /> <span>Sign in with GitHub</span>
-            </>
-          )}
-        </Button>
-
-        <div className="flex items-center gap-4 my-6">
-          <div className="flex-grow h-px bg-muted" />
-          <span className="text-muted-foreground text-sm whitespace-nowrap">
-            Or continue with
-          </span>
-          <div className="flex-grow h-px bg-muted" />
-        </div>
-
-        <div className="grid gap-3">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" name="email" placeholder="email@example.com" />
-          </div>
-
-          <Button>Continue with Email</Button>
-        </div>
-      </CardContent>
-    </Card>
-  );
+  return <LoginForm />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+'use client';
+
 import { ModeToggle } from '@/components/mode-toggle';
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import { authClient } from '@/lib/auth-client';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  const { data: session } = authClient.useSession();
+
+  async function SignOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push('/');
+        },
+      },
+    });
+  }
+
   return (
     <div className="max-w-[1000px] mx-auto mt-12 p-4">
       <h1>Modern LMS Platform</h1>
@@ -11,6 +29,14 @@ export default function Home() {
         molestiae similique vel, itaque, sequi molestias nam laborum temporibus
         quibusdam amet.
       </p>
+
+      {session ? (
+        <div className="flex gap-x-4 items-center">
+          Hello {session.user.name}!<Button onClick={SignOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
 
       <ModeToggle />
     </div>
